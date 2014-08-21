113a401673e905935af62bd628f752cf79e2a3e7
Justin Blair <jtblair@login4.stampede.tacc.utexas.edu>
2014-08-21 14:15:51 -0500

    First code to run/submit jobs on TACC - still problems in the mkdir section for some reason

---
+++ myBatchScript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH -J pTest            # Job Name
+#SBATCH -o pTest.o%j        # Output and error file name (%j expands to jobID)
+#SBATCH -n 16               # Total number of mpi tasks requested
+#SBATCH -p development      # Queue (partition) name -- normal, development, etc.
+#SBATCH -t 00:10:00         # Run time (hh:mm:ss)i
+
+ibrun parallelTest.o
---
+++ myTACCcode.C
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+#include <string.h>
+#include <sstream>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+using namespace std;
+
+int main (int argc, char* argv[]){
+
+    int ierr = 0, nrank, irank;
+    ierr = MPI_Init(&argc, &argv);
+    ierr = MPI_Comm_size(MPI_COMM_WORLD, &nrank);
+    ierr = MPI_Comm_rank(MPI_COMM_WORLD, &irank);
+
+    string runDir = "10m_batchrun";
+
+    fprintf(stdout, "Task %d checking in!", irank);
+
+    string fullRunPath = "$WORK/alice/"+runDir;
+    struct stat dirChecker;
+    if(stat(fullRunPath.c_str(), &dirChecker)!=0){
+        mkdir(fullRunPath.c_str(), 0777);
+    }else{
+        fprintf(stdout, "stat returned 0 for Task %d", irank);
+    }
+
+    string outputDir = "batch_";
+    ostringstream ss;
+    ss <<  irank;
+    outputDir += ss.str();
+
+    string fullOutPath = fullRunPath + "/" + outputDir;
+    if(stat(fullOutPath.c_str(), &dirChecker)!=0){
+        mkdir(fullOutPath.c_str(), 0777);
+    }
+
+
+}
