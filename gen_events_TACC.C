f91815c5f295a3df1bb2684025bd5d5b0dd67193
Justin Blair <jtblair@utexas.edu>
2015-02-24 11:35:02 -0600

    got rid of output of check messages

--- gen_events_TACC.C
+++ gen_events_TACC.C
@@ -39,10 +39,10 @@ int main (int argc, char* argv[]){
     string fullRunPath = "/scratch/03094/jtblair/alice/"+runDir;
     struct stat dirChecker;
     if(stat(fullRunPath.c_str(), &dirChecker)!=0){
-        fprintf(stdout, "Task %d fullRunPath: %s\n", irank, fullRunPath.c_str());
+//      fprintf(stdout, "Task %d fullRunPath: %s\n", irank, fullRunPath.c_str());
         mkdir(fullRunPath.c_str(), 0777);
     }else{
-        fprintf(stdout, "stat returned 0 for Task %d\n", irank);
+//      fprintf(stdout, "stat returned 0 for Task %d\n", irank);
     }
 
     string fullOutPath = fullRunPath + "/" + outputDir+"/";
@@ -52,7 +52,7 @@ int main (int argc, char* argv[]){
 
     string alirootCmd = "/work/03093/deepat/alice/aliroot/vAN-20140818/build/bin/tgt_linuxx8664gcc/aliroot -b -q \'/work/03094/jtblair/utaustin/aliroot/"+runFile+"("+numEvents+", \""+fullOutPath+outFile+"\", \"FASTGEN\")\' > /dev/null";
 
-    fprintf(stdout, "%s", alirootCmd.c_str());
+//  fprintf(stdout, "%s", alirootCmd.c_str());
     system(alirootCmd.c_str());
 
     ierr = MPI_Finalize();
