f4d119f4d9c24176e61d987bff44946bceb4f9da
Justin Blair <jbeta137@gmail.com>
2015-04-28 15:51:06 -0500

    Working to add k0 analysis to phi analysis

--- aliroot/histo_gen.C
+++ aliroot/histo_gen.C
@@ -7,15 +7,14 @@ using namespace std;
 #include "TSystem.h"
 #include "TClassTable.h"
 #include "TObjArray.h"
+#include "TStopwatch.h"
 
-void histo_gen(string inputDir, string inputFile, string outputDir, string outputFile){
-	TStopwatch timer_io;
-	timer_io.start();
-
+void histo_gen(string input_dir, string input_file, string output_dir, string output_file){
 	if (gClassTable->GetID("AliRun") < 0) {
 		gROOT->LoadMacro("loadlibs.C");
 		loadlibs();
     }
+    TStopwatch timer_io;
 
     gSystem->SetIncludePath("-I$ROOTSYS/include -I$ALICE_ROOT/include -I$ALICE_ROOT");
     gSystem->Load("libhijing.so");
@@ -30,32 +29,59 @@ void histo_gen(string inputDir, string inputFile, string outputDir, string outpu
     TH1F *phiPhiDist = new TH1F("PhiPhiDist", "phi meson angular phi distribution", 100, -0.1, 6.29);
     TH1F *hPhiDist = new TH1F("HPhiDist", "hadron angular phi distribution", 100, -0.1, 6.29);
     TH3F *DphiHPhi = new TH3F("DphiHPhi", "#Delta#phi correlation for Hadron-Phi", 1000, 0, 50, 1000, 0, 50, 64, -1.57, 4.71);
-    string inputFullPath = inputDir+"/"+inputFile;
-    AliRunLoader* rl = AliRunLoader::Open(inputFullPath.c_str());
+
+    TH2F *k0PhiDist = new TH2F("k0Dist", "Phi distribution for k0", 100, -0.1, 6.29);
+    TH3F *DphiHK0 = new TH3F("DphiHK0", "#Delta#phi correlation for Hadron-K^{0}", 1000, 0, 50, 1000, 0, 50, 64, -1.57, 4.71);
+
+    string input_full_path = input_dir+"/"+input_file;
+    AliRunLoader* rl = AliRunLoader::Open(input_full_path.c_str());
 
     if(!rl){
         fprintf(stderr, "Run Loader not initialized, stopping program.");
         return;
     }
 
-    Int_t numEvents = rl->GetNumberOfEvents();
+    Int_t num_events = rl->GetNumberOfEvents();
+    
     TDatabasePDG* DataBase = new TDatabasePDG();
 
-    printf("Number of Events: %d", numEvents);
+    printf("Number of Events: %d", num_events);
 
     rl->LoadKinematics();
     rl->LoadHeader();
+
     Int_t count = 0;
+    Int_t npart = 0;
+    Double_t pt=-1, E=-1, theta=-1, phi=-1, eta=-99, y=-99, eta_calc=-99, p_tot = 0, p_z = 0;
+    Double_t asso_pt=-1, asso_phi=-1, asso_eta=-9, delta_phi=-99;
+    Int_t par_pdg = 0, asso_pdg=0;
+    TParticle *particle, *hAsso;
+    AliStack* stack;
+
+    timer_io.Stop();
+    fprintf(stdout, "IO ");
+    timer_io.Print("u");
+
     //Loop over all events
-    for(Int_t nev=0; nev<numEvents; nev++){
-        if(nev%100 == 0)printf("Event: %d\n", nev);
+    for(Int_t nev=0; nev<num_events; nev++){
+//        if(nev%100 == 0)printf("Event: %d\n", nev);
+        TStopwatch timer_getevent;
+
         rl->GetEvent(nev);
-        AliStack* stack = rl->Stack();
-        Int_t npart = stack->GetNprimary();
-        Double_t pt=-1, E=-1, theta=-1, phi=-1, eta=-99, y=-99, eta_calc=-99, p_tot = 0, p_z = 0;
-        Double_t assoPt=-1, assoPhi=-1, assoEta=-9, Dphi=-99;
-        Int_t parPdg = 0, assoPdg=0;
-        TParticle *particle, *hAsso;
+        stack = rl->Stack();
+        
+        npart = stack->GetNprimary();
+        
+        //reset all variables before new event
+        pt=-1, E=-1, theta=-1, phi=-1, eta=-99, y=-99, eta_calc=-99, p_tot = 0, p_z = 0;
+        asso_pt= -1, asso_phi=-1, asso_eta=-9, delta_phi=-99;
+        par_pdg = 0, asso_pdg = 0;
+        
+        timer_getevent.Stop();
+        fprintf(stdout, "GETEVENT ");
+        timer_getevent.Print("u");
+        
+        TStopwatch timer_analysis;
         //loop over all particles in stack
         for(Int_t part=0; part<npart; part++){
             particle = stack->Particle(part);
@@ -66,28 +92,30 @@ void histo_gen(string inputDir, string inputFile, string outputDir, string outpu
             eta = particle->Eta();
             p_tot = particle->P();
             p_z = particle->Pz();
-            parPdg = particle->GetPdgCode();
+            par_pdg = particle->GetPdgCode();
             eta_calc = 0.5*TMath::Log((p_tot + p_z)/(p_tot - p_z));
             //select only hadrons within the eta range -0.9 < eta <0.9 and pt > 150 MeV
-            //if(nev%1000==0 && TMath::Abs(eta_calc)<1) printf("  Particle eta: %f, Calculated eta: %f, Pt: %f, PDG: %d\n", eta, eta_calc, pt, parPdg);
-            if((TMath::Abs(parPdg)==2212 || TMath::Abs(parPdg)==211 || TMath::Abs(parPdg)==321 || TMath::Abs(parPdg)==11 || TMath::Abs(parPdg)==333)&&(TMath::Abs(eta_calc)<0.9)&&(pt > 0.150)){
+            //if(nev%1000==0 && TMath::Abs(eta_calc)<1) printf("  Particle eta: %f, Calculated eta: %f, Pt: %f, PDG: %d\n", eta, eta_calc, pt, par_pdg);
+            if((TMath::Abs(par_pdg)==2212 || TMath::Abs(par_pdg)==311 || TMath::Abs(par_pdg)==211 || TMath::Abs(par_pdg)==321 || TMath::Abs(par_pdg)==11 || TMath::Abs(par_pdg)==333)&&(TMath::Abs(eta_calc)<0.9)&&(pt > 0.150)){
                 hadronPt->Fill(pt);
                 count++;
-                if(TMath::Abs(parPdg)==333){                  
+                if(TMath::Abs(par_pdg)==333){                  
                     phiPt->Fill(pt);
                     phiPhiDist->Fill(phi);
+                }else if(TMath::Abs(par_pdg)==311){
+                    k0PhiDist->Fill(phi);
                 }else{
                     hPhiDist->Fill(phi);
                     //loop over all particles that aren't this hadron to compute delta-phi
                     for(Int_t apart = 0; apart<npart; apart++){
                         if(apart == part) continue;
                         hAsso = stack->Particle(apart);
-                        assoPt = hAsso->Pt();
-                        assoPhi = hAsso->Phi();
-                        assoEta = hAsso->Eta();
-                        assoPdg = hAsso->GetPdgCode();
+                        asso_pt = hAsso->Pt();
+                        asso_phi = hAsso->Phi();
+                        asso_eta = hAsso->Eta();
+                        asso_pdg = hAsso->GetPdgCode();
                         //select just phi mesons in the eta range: |eta| < 0.9 and pt > 150 MeV
-                        if((TMath::Abs(assoPdg)==333) && (TMath::Abs(assoEta)< 0.9) && (assoPt > 0.150)){
+                        if((TMath::Abs(asso_pdg)==333 || TMath::Abs(asso_pdg)==311) && (TMath::Abs(asso_eta)< 0.9) && (asso_pt > 0.150)){
                             //printf("Got Here! \n");
                             //check that hadron isn't daughter particle of any phi meson
                             Int_t numDaughters = hAsso->GetNDaughters();
@@ -100,37 +128,58 @@ void histo_gen(string inputDir, string inputFile, string outputDir, string outpu
                                 }
                             }
                             */
-                            Int_t firstMotherIndex = particle->GetMother(0); //we check to make sure the hadron isn't the daughter of any phi meson
+                            Int_t firstMotherIndex = particle->GetMother(0); //we check to make sure the hadron isn't the daughter of any phi meson (k0)
                             Int_t secondMotherIndex = particle->GetMother(1);
-                            if(firstMotherIndex > 0){
-                                TParticle *firstmother = stack->Particle(firstMotherIndex);
-                                if(TMath::Abs(firstmother->GetPdgCode()) == 333) isHadronDaughter = true;
-                            }
-                            if(secondMotherIndex > 0){
-                                TParticle *lastmother = stack->Particle(secondMotherIndex);
-                                if(TMath::Abs(lastmother->GetPdgCode()) == 333) isHadronDaughter = true;
+                            if(TMath::Abs(asso_pdg)==333){
+                                if(firstMotherIndex > 0){
+                                    TParticle *firstmother = stack->Particle(firstMotherIndex);
+                                    if(TMath::Abs(firstmother->GetPdgCode()) == 333) isHadronDaughter = true;
+                                }
+                                if(secondMotherIndex > 0){
+                                    TParticle *lastmother = stack->Particle(secondMotherIndex);
+                                    if(TMath::Abs(lastmother->GetPdgCode()) == 333) isHadronDaughter = true;
+                                }
+                            }else if(TMath::Abs(asso_pdg)==311){
+                                if(firstMotherIndex > 0){
+                                    TParticle *firstmother = stack->Particle(firstMotherIndex);
+                                    if(TMath::Abs(firstmother->GetPdgCode()) == 311) isHadronDaughter = true;
+                                }
+                                if(secondMotherIndex > 0){
+                                    TParticle *lastmother = stack->Particle(secondMotherIndex);
+                                    if(TMath::Abs(lastmother->GetPdgCode()) == 311) isHadronDaughter = true;
+                                }
                             }
                             if(isHadronDaughter) continue;
-                            Dphi = phi - assoPhi;
-                            if(Dphi >3*TMath::Pi()/2){
-                                Dphi = Dphi - 2*TMath::Pi();
+                            delta_phi = phi - asso_phi;
+                            if(delta_phi >3*TMath::Pi()/2){
+                                delta_phi = delta_phi - 2*TMath::Pi();
                             }
-                            if(Dphi < -TMath::Pi()/2){
-                                Dphi = Dphi + 2*TMath::Pi();
+                            if(delta_phi < -TMath::Pi()/2){
+                                delta_phi = delta_phi + 2*TMath::Pi();
                             }
-                            DphiHPhi->Fill(pt, assoPt, Dphi);
+                            if(TMath::Abs(asso_pdg)==333){
+                                DphiHPhi->Fill(pt, asso_pt, delta_phi);
+                            }else{
+                                DphiHK0->Fill(pt, asso_pt, delta_phi);
+                            }
                         }
                     }
                 }
             }
         }
+        timer_analysis.Stop();
+        fprintf(stdout, "ANALYSIS ");
+        timer_analysis.Print("u");
     }
+
+    TStopwatch timer_write;
+
 //    printf("Total hadrons counted in eta range: %d\n", count);
     rl->~AliRunLoader(); 
 
 
     //open new file to contain histograms
-    string outputFullPath = outputDir+"/"+outputFile;
+    string outputFullPath = output_dir+"/"+output_file;
     TFile *histoOutput = new TFile(outputFullPath.c_str(), "RECREATE");
     histoOutput->cd();
 
@@ -141,4 +190,7 @@ void histo_gen(string inputDir, string inputFile, string outputDir, string outpu
 
     histoOutput->Close();
 
+    timer_write.Stop();
+    fprintf(stdout, "WRITE ");
+    timer_write.Print("u");
 }
--- gen_histos_carina.C
+++ gen_histos_carina.C
@@ -16,22 +16,23 @@ int main (int argc, char* argv[]){
     ierr = MPI_Comm_size(MPI_COMM_WORLD, &nrank);
     ierr = MPI_Comm_rank(MPI_COMM_WORLD, &irank);
 
-    string input_dir_root, input_dir, input_file, output_dir, output_file;
+    string input_dir_root, input_dir, input_file, output_dir, output_file_root, output_file;
     string run_file = "/Users/jtblair/utaustin/aliroot/histo_gen.C";
     string aliroot_cmd, aliroot_source="ali";
     int num_processors, first_dir, last_dir, range, per_processor;
 
     if(argc != 8){
         printf("Incorrect number of arguments.  Correct usage is: \n");
-        printf("gen_histos(input_dir_root, input_file, output_dir, output_file, num_processors, first_dir, last_dir)\n");
-        return 0;
+        printf("gen_histos(input_dir_root, input_file, output_dir, output_file_root, num_processors, first_dir, last_dir)\n");
+        ierr = MPI_Finalize();
+        return ierr;
     }else{
 
         input_dir_root.assign(argv[1], find(argv[1], argv[1]+255, '\0'));
 
         input_file.assign(argv[2], find(argv[2], argv[2]+255, '\0'));
         output_dir.assign(argv[3], find(argv[3], argv[3]+255, '\0'));
-        output_file.assign(argv[4], find(argv[4], argv[4]+255, '\0'));
+        output_file_root.assign(argv[4], find(argv[4], argv[4]+255, '\0'));
 
         num_processors = atoi(argv[5]);
         first_dir = atoi(argv[6]);
@@ -40,7 +41,8 @@ int main (int argc, char* argv[]){
         range = last_dir - first_dir + 1;
         if(range <= 0){
             fprintf(stderr, "Error: first_dir must be less than or equal to last_dir number");
-            return 1;
+            ierr = MPI_Finalize();
+            return ierr;
         }
 
         per_processor = ceil((double)(range)/(double)(num_processors));
@@ -50,7 +52,7 @@ int main (int argc, char* argv[]){
             mkdir(output_dir.c_str(), 0777);
         }         
 
-        system(aliroot_source.c_str());
+//        system(aliroot_source.c_str());
 
         for(int i = first_dir+(irank*per_processor); i < first_dir+((irank+1)*per_processor); i++){
             if(i > last_dir)break;
@@ -58,9 +60,12 @@ int main (int argc, char* argv[]){
             ss << i;
             input_dir = input_dir_root + ss.str();
 
+            output_file = output_file_root + ss.str()+".root";
+
             string aliroot_cmd = "aliroot -b -l -q \'"+run_file+"(\""+input_dir+"\", \""+input_file+"\",\""+output_dir+"\", \""+output_file+"\")\'";
             system(aliroot_cmd.c_str());
         }
         ierr = MPI_Finalize();
         return ierr;
     }
+}
