05e31941f8c41db7d9fb2d543a4b431da18f51d6
Justin Blair <jbeta137@gmail.com>
2015-06-10 11:15:59 -0500

    reran script

--- carinascript.sh
+++ carinascript.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-NUM_PROCESSORS=8
+NUM_PROCESSORS=1
 INPUT_DIR_ROOT="/Volumes/Data2/jtblair/20150303_100m_batchrun_2/batch_"
 INPUT_FILE="25k_minbias.root"
 OUTPUT_DIR="/Users/jtblair/invmass_test/"
 OUTPUT_FILE_ROOT="histo_"
-FIRST=0
-LAST=3999
-WRITE_FILE="/Users/jtblair/output12.txt"
+FIRST=3630
+LAST=3632
+WRITE_FILE="/Users/jtblair/output13.txt"
 RUN_FILE="/Users/jtblair/utaustin/invMassGenerator.o"
 
 nohup mpirun -np $NUM_PROCESSORS $RUN_FILE $INPUT_DIR_ROOT $INPUT_FILE $OUTPUT_DIR $OUTPUT_FILE_ROOT $NUM_PROCESSORS $FIRST $LAST > $WRITE_FILE &
