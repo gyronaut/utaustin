739aca34923eda140476a7b2cc813a7d6a9ef452
Justin Blair <jbeta137@gmail.com>
2017-06-12 16:37:21 -0500

    fixup! updated task for mult bins, testing high invmass behavior

--- phiCorrelations/RunMacro.C
+++ phiCorrelations/RunMacro.C
@@ -13,16 +13,16 @@ void RunMacro()
 
    // Firstly, set some variables
    const char* launch = "grid"; // grid, local (if your data is on your local machine, doesn't connect at all)
-   const char*  mode = "terminate"; //test, full, terminate  (test= connect to grid but run locally, full= run on grid, terminate= merge output on grid)
-   Bool_t pre_final_stage = kFALSE; //TRUE = merging done on grid, FALSE = merge happens locally   
+   const char*  mode = "test"; //test, full, terminate  (test= connect to grid but run locally, full= run on grid, terminate= merge output on grid)
+   Bool_t pre_final_stage = kTRUE; //TRUE = merging done on grid, FALSE = merge happens locally   
    Int_t cyclenumber = 1;
    Bool_t debug = kTRUE;
    char* work_dir = "PhiCorrelations_LHC16q";
-   char* output_dir = "output_2017_05_14";
+   char* output_dir = "output_2017_06_12";
    Int_t ttl = 50000;
    Int_t noffiles = 20;
-   Int_t runcycle[]={0,32};
-//   Int_t runcycle[]={0,1,5,11,18,24,32};
+//   Int_t runcycle[]={0,32};
+   Int_t runcycle[]={0,1,5,11,18,24,32};
    Bool_t UseParfiles = kFALSE;
 
 // create and customize the alien handler
